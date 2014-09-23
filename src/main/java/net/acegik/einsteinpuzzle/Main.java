769d058b1a8346090b080a0cb4d15b9dc35f04b3
drupalex <drupalex@drupalex-ubuntu>
2014-09-23 09:41:28 +0700

    Change comment on genNextPermutation() method

--- src/main/java/net/acegik/einsteinpuzzle/Main.java
+++ src/main/java/net/acegik/einsteinpuzzle/Main.java
@@ -84,19 +84,18 @@ public class Main {
         if (n < 2) return false;
 
         // tìm j là chỉ số lớn nhất mà a[j] < a[j+1]
-        int j = n - 1;
-        for(j=n-1; j>0; j--) if (a[j-1] < a[j]) break;
-        if (j == 0) return false;
-        j--;
+        int j;
+        for(j=n-2; j>=0; j--) if (a[j] < a[j+1]) break;
+        if (j == -1) return false;
 
-        // tìm k
-        int k = n - 1;
+        // tìm k là chỉ số lớn nhất mà a[j] < a[k]
+        int k;
         for(k=n-1; k>j; k--) if (a[k] > a[j]) break;
 
-        // đổi chỗ a[j] và a[k]
+        // hoán đổi giá trị a[j] và a[k]
         int t = a[j]; a[j] = a[k]; a[k] = t;
 
-        // đảo danh sách sau a[j]
+        // đảo danh sách sau a[j] (từ a[j+1] đến hết)
         int s = j + 1;
         int r = n - 1;
 
